80266dd62669f182c71f52012d7fd201a2d5e4cf
Michael Ahari <michael.ahari@btinternet.com>
2016-02-26 10:38:33 +0000

    retrieved most recent commit from most recently created repository

--- htb-commit-scraper.py
+++ htb-commit-scraper.py
@@ -5,6 +5,9 @@ from flask.json import jsonify
 import os
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+import dateutil.parser
+import datetime
+import pytz
 
 
 # create application
@@ -29,7 +32,7 @@ def demo():
     github = OAuth2Session(client_id, redirect_uri=redirect_uri, scope = ['repo'])
     authorization_url, state = github.authorization_url(authorization_base_url)
 
-    # State is used to prevent CSRF, keep this for later.
+    # State is used to prevent CSRF, keep this for later
     session['oauth_state'] = state
     return redirect(authorization_url)
 
@@ -49,28 +52,69 @@ def callback():
     token = github.fetch_token(token_url, client_secret=client_secret,
                                authorization_response=request.url)
 
+    session['oauth_token'] = token
+
     # At this point you can fetch protected resources but lets save
     # the token and show how this is done from a persisted token
     # in /profile.
-    session['oauth_token'] = token
-    print session['oauth_token']
-    #add token to list of tokens
-    tokens.append(token)
 
     return redirect(url_for('commits'))
+    #return json_r['commit']['author']['date']
+    #return ''#json_r['commit']['author']['date']
+
+
+    #print session['oauth_token']
+    #add token to list of tokens
+    #tokens.append(token)
+
+    #return redirect(url_for('commits'))
 
 @app.route("/commits", methods=["GET"])
 def commits():
     OAUTH_KEY = session['oauth_token']
-    owner = 'MichaelAhari'
-    repo = 'htb-commit-scraper'
-    print 'here'
-    #r = requests.get('http://api.github.com/repos/MichaelAhari/htb-commit-scraper/commits', auth=('access_token', OAUTH_KEY))
-    r = requests.get('https://api.github.com/users/MichaelAhari')# auth=('token', OAUTH_KEY))
-    json_data = r.json()
-    print json_data['login']
-    return ''
+    OWNER = ''
+    REPO = ''
 
+    #authorise user using token
+    github = OAuth2Session(client_id, token=OAUTH_KEY)
+
+    #get user details including username and repo
+    user_info = github.get('https://api.github.com/user')
+    json_ui = user_info.json()
+
+    OWNER = json_ui['login']
+
+    #get repos
+    repo_info = github.get('https://api.github.com/user/repos')
+
+    """Get the most recent repository using created_at key"""
+    dates = []
+    for repo in repo_info.json():
+        #add date of creation to list, converting from ISO8601 to datetime python type
+        dates.append(dateutil.parser.parse(repo['created_at']))
+
+    current_date = datetime.datetime.now(pytz.utc)
+    DATE_CREATED = max(date for date in dates if date < current_date)
+
+    #get latest repo name
+    for repo in repo_info.json():
+        if dateutil.parser.parse(repo['created_at']) == DATE_CREATED:
+            REPO = repo['name']
+
+    #REPO = latest_repo['name']
+
+    """Get latest commit to REPO"""
+    commit = (github.get('https://api.github.com/repos/%s/%s/commits/master' % (OWNER, REPO))).json()
+
+    commit_date = dateutil.parser.parse(commit['commit']['author']['date'])
+
+    #check if commit has been made in the last 5 minutes
+    if (commit_date - current_date).total_seconds() < (5*60):
+        commit_message = commit['commit']['message']
+        session['commit_message'] = commit_message
+
+
+    return commit_message
 if __name__ == '__main__':
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     app.secret_key = os.urandom(24)
