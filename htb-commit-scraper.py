bc6405a638b600fbaa1869e2c01bbd5abf8426cc
Michael Ahari <michael.ahari@btinternet.com>
2016-02-27 00:08:50 +0000

    Set up postgresql server and retrieves most recent commit from the newest affiliated repository

--- htb-commit-scraper.py
+++ htb-commit-scraper.py
@@ -8,11 +8,38 @@ from flask import Flask, request, session, g, redirect, url_for, \
 import dateutil.parser
 import datetime
 import pytz
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import JSON
+from flask import make_response
 
 
 # create application
 app = Flask(__name__)
 app.config.from_object(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:frenNi31@178.62.99.27/users'
+db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(30))
+    oauthkey = db.Column(JSON)
+    repo = db.Column(db.String(30))
+    owner = db.Column(db.String(30))
+
+    def __init__(self, login, oauthkey, repo, owner):
+        self.login = login
+        self.oauthkey = oauthkey
+        self.repo = repo
+        self.owner = owner
+
+    def __repr__(self):
+        return '<User %r>' % self.login
+
+#create database
+db.create_all()
+
 
 client_id = "f9ba003f3e1eba4ff7f5"
 client_secret = "78dc535324b5e6fd8699748df2140f268147a8e4"
@@ -20,10 +47,15 @@ authorization_base_url = 'https://github.com/login/oauth/authorize'
 token_url = 'https://github.com/login/oauth/access_token'
 redirect_uri = 'http://localhost:5000/callback'
 
-tokens = []
 
 @app.route("/")
-def demo():
+def home():
+
+
+#    if 'oauth' in request.cookies:
+#        return redirect(url_for('callback'))
+#    else:
+
     """Step 1: User Authorization.
 
     Redirect the user/resource owner to the OAuth provider (i.e. Github)
@@ -35,12 +67,17 @@ def demo():
     # State is used to prevent CSRF, keep this for later
     session['oauth_state'] = state
     return redirect(authorization_url)
-
+    response = make_response(redirect(authorization_url))
+    response.set_cookie('oauth_state', state)
+    return response
 
 # Step 2: User authorization, this happens on the provider.
 
 @app.route("/callback", methods=["GET"])
 def callback():
+
+    #if 'oauth_state' not in request.cookies:
+    #    return redirect(url_for('home'))
     """ Step 3: Retrieving an access token.
 
     The user has been redirected back from the provider to your registered
@@ -52,28 +89,13 @@ def callback():
     token = github.fetch_token(token_url, client_secret=client_secret,
                                authorization_response=request.url)
 
-    session['oauth_token'] = token
+    #session['oauth_token'] = token
 
     # At this point you can fetch protected resources but lets save
     # the token and show how this is done from a persisted token
     # in /profile.
 
-    return redirect(url_for('commits'))
-    #return json_r['commit']['author']['date']
-    #return ''#json_r['commit']['author']['date']
-
-
-    #print session['oauth_token']
-    #add token to list of tokens
-    #tokens.append(token)
-
-    #return redirect(url_for('commits'))
-
-@app.route("/commits", methods=["GET"])
-def commits():
-    OAUTH_KEY = session['oauth_token']
-    OWNER = ''
-    REPO = ''
+    OAUTH_KEY = token
 
     #authorise user using token
     github = OAuth2Session(client_id, token=OAUTH_KEY)
@@ -82,7 +104,7 @@ def commits():
     user_info = github.get('https://api.github.com/user')
     json_ui = user_info.json()
 
-    OWNER = json_ui['login']
+    LOGIN = json_ui['login']
 
     #get repos
     repo_info = github.get('https://api.github.com/user/repos')
@@ -100,18 +122,43 @@ def commits():
     for repo in repo_info.json():
         if dateutil.parser.parse(repo['created_at']) == DATE_CREATED:
             REPO = repo['name']
+            OWNER = repo['owner']['login']
 
-    #REPO = latest_repo['name']
+    #add record to database
+    new_record = User(LOGIN, OAUTH_KEY, REPO, OWNER)
 
-    """Get latest commit to REPO"""
-    commit = (github.get('https://api.github.com/repos/%s/%s/commits/master' % (OWNER, REPO))).json()
+    User.query.filter_by(login=LOGIN).delete()
 
-    commit_date = dateutil.parser.parse(commit['commit']['author']['date'])
+    db.session.add(new_record)
+    db.session.commit()
 
-    #check if commit has been made in the last 5 minutes
-    if (commit_date - current_date).total_seconds() < (5*60):
-        commit_message = commit['commit']['message']
-        session['commit_message'] = commit_message
+    response = make_response(redirect(url_for('commits')))
+    response.set_cookie('oauth', 'authorized')
+    return response
+
+@app.route("/commits", methods=["GET"])
+def commits():
+
+
+    #get all users from database
+    users = User.query.all()
+
+    for user in users:
+
+        LOGIN = user.login
+        OAUTH_KEY = user.oauthkey
+        REPO = user.repo
+        OWNER = user.owner
+
+        """Get latest commit to REPO"""
+        github = OAuth2Session(client_id, token=OAUTH_KEY)
+        commit = (github.get('https://api.github.com/repos/%s/%s/commits/master' % (OWNER, REPO))).json()
+        commit_date = dateutil.parser.parse(commit['commit']['author']['date'])
+
+        #check if commit has been made in the last 5 minutes
+        current_date = datetime.datetime.now(pytz.utc)
+        if (commit_date - current_date).total_seconds() < (5*60):
+            commit_message = commit['commit']['message']
 
 
     return commit_message
